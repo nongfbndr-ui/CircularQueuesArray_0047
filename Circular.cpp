57af8e72d71abc39a8ff55060da98fbb616a8d8c
Moreno <nongfbndr@gmail.com>
2026-05-28 16:00:49 +0700

    membuat remove

--- Circular.cpp
+++ Circular.cpp
@@ -46,4 +46,12 @@ public:
         queue_array[REAR] = num;
     }
 
-    
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue Underflow!";
+            return;
+        }
+        
