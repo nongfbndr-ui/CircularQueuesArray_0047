a6e9db0ce55fe978db10de850be3b7a4d7be1a5b
Moreno <nongfbndr@gmail.com>
2026-05-28 16:04:47 +0700

    tambahkan riset

--- Circular.cpp
+++ Circular.cpp
@@ -56,4 +56,20 @@ public:
         }
         cout << "Element deleted from the queue is: " << queue_array[FRONT] << endl;
 
-        
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+
+    
